2be7083ac8cfff4ab944944e6c890123017866af
jjr-chaine <105772517+jjr-chaine@users.noreply.github.com>
2022-09-27 19:59:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -69,15 +69,15 @@ const USER_CONFIG = {
       // 你想对他发送的模板消息的模板ID
       useTemplateId: '',
       // 所在省份
-      province: '广东',
+      province: '江苏',
       // 所在城市
-      city: '肇庆',
+      city: '南京',
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '10-03',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: 'https://wangxinleo.cn',
+      openUrl: 'https://www.bilibili.com/',
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
@@ -98,7 +98,7 @@ const USER_CONFIG = {
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-10-03' },
+        { keyword: 'love_day', date: '2017-10-03' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-10-03' },
         // 退伍日
@@ -172,13 +172,13 @@ const USER_CONFIG = {
     },
     {
       name: '老婆1',
-      id: '',
+      id: 'Lovo1zZ',
       useTemplateId: '',
-      province: '',
-      city: '',
-      horoscopeDate: '',
-      horoscopeDateType: '',
-      openUrl: 'https://wangxinleo.cn',
+      province: '江苏',
+      city: '南京',
+      horoscopeDate: '07-03',
+      horoscopeDateType: '巨蟹座',
+      openUrl: 'https://www.bilibili.com/',
       festivals: [],
       customizedDateList: [],
       courseSchedule: null
